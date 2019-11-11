4d868ba7736e8733f06519e2c732936961a758f9
zhangmk <51670196@qq.com>
2019-11-11 17:43:43 +0800

    axios

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 const DashBoard = () => {
-  return <Button>DashBoard</Button>;
+  const [data, setData] = useState({ hits: [] });
+  // const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "http://hn.algolia.com/api/v1/search?query=redux"
+      );
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Button onClick={() => console.log(data)}>DashBoard</Button>
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default DashBoard;
