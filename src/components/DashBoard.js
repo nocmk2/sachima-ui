dbb9861bab3f2768e04473340d79d724d2e28b33
zhangmk <51670196@qq.com>
2019-11-12 17:44:42 +0800

    sachima  api

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
+import { TextField } from "@material-ui/core";
 
 const DashBoard = () => {
   const [data, setData] = useState({ hits: [] });
-  // const [count, setCount] = useState(0);
+  const [query, setQuery] = useState("redux");
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        "http://hn.algolia.com/api/v1/search?query=redux"
+        `http://hn.algolia.com/api/v1/search?query=${query}`
       );
       setData(result.data);
     };
 
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <>
       <Button onClick={() => console.log(data)}>DashBoard</Button>
+      <TextField
+        value={query}
+        onChange={e => {
+          setQuery(e.target.value);
+        }}
+      />
       <ul>
         {data.hits.map(item => (
           <li key={item.objectID}>
--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 const Rules = () => {
-  return <Button>Rules</Button>;
+  const [data, setData] = useState("ddd");
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("http://localhost:8000/test");
+      console.log(result.data);
+      console.log(result.data.text);
+      setData(result.data.text);
+    };
+
+    fetchData();
+  }, []);
+
+  return <Button>{data}</Button>;
 };
 
 export default Rules;
