7f9d1e8d455ec59a316efea79aa276d0d83e9599
zhangmk <51670196@qq.com>
2019-11-15 13:45:06 +0800

    Modal popup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Backdrop from "@material-ui/core/Backdrop";
+import Modal from "@material-ui/core/Modal";
 import Button from "@material-ui/core/Button";
 import SpeedDial from "@material-ui/lab/SpeedDial";
 import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
@@ -13,9 +14,14 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import { useHistory } from "react-router-dom";
 import { withRouter } from "react-router";
 import Login from "./Login";
-import axios from "axios";
+import Fade from "@material-ui/core/Fade";
 
 const useStyles = makeStyles(theme => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
   root: {
     flexGrow: 1
   },
@@ -23,6 +29,12 @@ const useStyles = makeStyles(theme => ({
     position: "absolute",
     bottom: theme.spacing(1),
     right: theme.spacing(1)
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
   }
 }));
 
@@ -38,8 +50,9 @@ const Main = props => {
   const history = useHistory();
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [openBack, setOpenBack] = useState(false);
   const [hidden, setHidden] = useState(false);
-  const [token, setToken] = useState("");
+  // const [token, setToken] = useState("");
   const [logStatus, setLogStatus] = useState(true);
 
   const handleVisibility = () => {
@@ -54,6 +67,10 @@ const Main = props => {
     setOpen(false);
   };
 
+  const handleBackClose = () => {
+    setOpenBack(false);
+  };
+
   const handleClick = name => {
     console.log(name);
     history.push("/" + name);
@@ -61,11 +78,13 @@ const Main = props => {
 
   const handleLogClick = () => {
     if (logStatus) {
-      setToken("");
+      // setToken("");
+      localStorage.setItem("token", "");
       setLogStatus(false);
     } else {
       // TODO: popup login page
-      alert("login page");
+      setOpenBack(true);
+      // alert("login");
     }
   };
 
@@ -73,9 +92,29 @@ const Main = props => {
     <div className={classes.root}>
       <Button onClick={handleVisibility}>Sachima</Button>
       <Button onClick={handleLogClick}>{logStatus ? "LogOut" : "LogIn"}</Button>
-      <div style={{ color: "gray" }}>{token}</div>
+      {/* <div style={{ color: "gray" }}>{token}</div> */}
       {props.children}
       <Backdrop open={open} />
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={openBack}
+        onClose={handleBackClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 1000
+        }}
+      >
+        <Fade in={openBack}>
+          <div className={classes.paper}>
+            <Login />
+          </div>
+        </Fade>
+      </Modal>
+
       <SpeedDial
         ariaLabel="SpeedDial tooltip"
         className={classes.speedDial}
