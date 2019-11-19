3ad39c6efa38069a31a85ff4fdff2292eed139a1
zhangmk <51670196@qq.com>
2019-11-19 13:42:36 +0800

    login mes

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,15 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
 import VerifiedUser from "@material-ui/icons/VerifiedUser";
-import Typography from "@material-ui/core/Typography";
+// import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Snackbar from '@material-ui/core/Snackbar';
+import Slide from '@material-ui/core/Slide';
 import axios from "axios";
 
 const useStyles = makeStyles(theme => ({
@@ -42,6 +44,23 @@ const Login = () => {
     const [user, setUser] = useState({ email: "", password: "" });
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [open, setOpen] = React.useState(false);
+    const [transition, setTransition] = React.useState(undefined);
+    const [message, setMessage] = useState("")
+
+    function TransitionRight(props) {
+        return <Slide {...props} direction="right" />;
+    }
+
+    const sendMessage = (Transition, mes) => {
+        setTransition(() => Transition);
+        setOpen(true);
+        setMessage(mes)
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -61,24 +80,33 @@ const Login = () => {
         setUser({ ...user, [event.target.name]: event.target.value });
     };
 
+    const login = async () => {
+        try {
+            const result = await axios({
+                method: "post",
+                url: "http://localhost:8000/login",
+                data: {
+                    username: user.email,
+                    password: user.password
+                }
+            });
+            localStorage.setItem("token", result.data.token);
+            sendMessage(TransitionRight, "登陆成功，欢迎" + user.email)
+        } catch (err) {
+            console.log(err)
+            sendMessage(TransitionRight, "登陆失败，请重试。或联系管理员")
+        }
+        setLoading(false)
+    };
+
     useEffect(() => {
         if (loading) {
             login();
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [loading]);
 
-    const login = async () => {
-        const result = await axios({
-            method: "post",
-            url: "http://localhost:8000/login",
-            data: {
-                username: user.email,
-                password: user.password
-            }
-        });
-        localStorage.setItem("token", result.data.token);
-        setLoading(false)
-    };
+
 
     return (
         <Container component="main" maxWidth="xs">
@@ -136,6 +164,15 @@ const Login = () => {
                         提交
           </Button>
                 </form>
+                <Snackbar
+                    open={open}
+                    onClose={handleClose}
+                    TransitionComponent={transition}
+                    ContentProps={{
+                        'aria-describedby': 'message-id',
+                    }}
+                    message={<span id="message-id">{message}</span>}
+                />
             </div>
         </Container>
     );
