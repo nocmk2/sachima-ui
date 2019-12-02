53df8acbecd0111005e35026263c4c0550544f1d
zhangmk <51670196@qq.com>
2019-12-02 11:13:43 +0800

    axios interceptor

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -18,8 +18,23 @@ const Rules = () => {
   const [querytime, setQueryTime] = useState(Now());
   const [features, setFeatures] = useState([]);
 
+  axios.interceptors.response.use(response => {
+    return response;
+  }, error => {
+    if (error.response.status === 401) {
+      //place your reentry code
+      dispatch({ type: "sendMessage", newMessage: { open: true, move: "left", info: "您没有权限,请登陆" } })
+    }
+    return error;
+  });
+
   // simple get
   useEffect(() => {
+    // const abortController = new AbortController()
+    // const signal = abortController.signal
+    const signal = axios.CancelToken.source();
+
+
     const fetchData = async () => {
       setIsLoading(true);
       try {
@@ -27,7 +42,7 @@ const Rules = () => {
           method: "GET",
           url: url,
           headers: { Authorization: "Bearer " + localStorage.token }
-        });
+        }, { cancelToken: signal.token });
         console.log(result.data);
         console.log(result.data.text);
         setData(result.data.text);
@@ -36,16 +51,24 @@ const Rules = () => {
         }
         setIsLoading(false);
       } catch (error) {
+
         console.log(error)
-        setIsLoading(false);
-        if (error.response.status === 401) {
-          dispatch({ type: "sendMessage", newMessage: { open: true, move: "left", info: "您没有权限,请登陆" } })
+        if (axios.isCancel(error)) {
+          console.log('Error: ', error.message); // => prints: Api is being canceled
+        } else {
+          setIsLoading(false);
         }
+        // if (error.response.status === 401) {
+        //   dispatch({ type: "sendMessage", newMessage: { open: true, move: "left", info: "您没有权限,请登陆" } })
+        // }
         return error
       }
     };
 
     fetchData();
+    return function cleanup() {
+      signal.cancel("Cancelling in cleanup")
+    }
     // eslint-disable-next-line
   }, [url, querytime]);
 
