a007c44563eb9fa49793a75970084ab18537c27b
zhangmk <51670196@qq.com>
2020-03-29 13:15:17 +0800

    clean no use

--- src/utils/message.js
+++
@@ -1,9 +0,0 @@
-
-// import { useStateValue } from "../utils/state"
-
-// const [{ sachima }, dispatch] = useStateValue();
-// const SendMessage = info => {
-//     dispatch({ type: "sendMessage", newMessage: { open: true, move: "down", info: info } })
-// }
-
-// export default SendMessage
