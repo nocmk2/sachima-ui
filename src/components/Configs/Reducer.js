c2d6a8897b0bef4390de8ae55752405130fc4a17
zhangmk <51670196@qq.com>
2020-04-18 15:50:51 +0800

    switch

--- src/components/Configs/Reducer.js
+++ src/components/Configs/Reducer.js
@@ -5,28 +5,30 @@ export const initialState = {
     refs: {},
     // relation -> [{ start: userid, end: roleid }]
     relation: [],
-    users: [{ id: '23ijg5jg', name: 'ekijfejfoj' }, { id: 'mao', name: '猫' }],
+    users: [{ id: 'lllll Pvi', name: '初始值' }, { id: 'mao', name: '猫' }],
     roles: [],
     objects: []
 }
 
 export const reducer = (state, action) => {
     const { refs, relation } = state
-    if (action.type === "SETREFS") {
-        return { ...state, refs: action.payload }
-    } else if (action.type === "SETRELATION") {
-        return { ...state, relation: action.payload }
-    } else if (action.type === "DRAWLINE") {
-        DrawLineX(refs, relation)
-        return state
-    } else if (action.type === "DISPOSELINE") {
-        DisposeLine()
-        return state
-    } else if (action.type === 'ADD_USER') {
-        return { ...state, users: [{ id: '1', name: '1' }] }
-    } else if (action.type === 'SET_USERS') {
-        return { ...state, users: action.payload }
-    } else {
-        throw new Error(action.type + ': no such action!');
+    switch (action.type) {
+        case "SETREFS":
+            return { ...state, refs: action.payload }
+        case "SETRELATION":
+            return { ...state, relation: action.payload }
+        case "DRAWLINE":
+            DrawLineX(refs, relation)
+            return state
+        case "DISPOSELIN":
+            DisposeLine()
+            return state
+        case 'ADD_USER':
+            return { ...state, users: [{ id: '1', name: '1' }] }
+        case 'SET_USERS':
+            return { ...state, users: action.payload }
+        default:
+            return state
     }
+
 }
