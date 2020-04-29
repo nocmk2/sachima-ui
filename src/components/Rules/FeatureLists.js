e70274324f2e48cec5ad7f1464c04a0807567bab
zhangmk <51670196@qq.com>
2020-04-29 11:45:38 +0800

    clean

--- src/components/Rules/FeatureLists.js
+++ src/components/Rules/FeatureLists.js
@@ -19,16 +19,16 @@ import { useStateValue } from '../../utils/state'
 import TabPanel from '../TabPanel'
 import Card from "@material-ui/core/Card";
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import Slide from '@material-ui/core/Slide';
+// import Slide from '@material-ui/core/Slide';
 // import bgpic from '/public/contemplative-reptile.jpg';
-import { Motion, spring } from 'react-motion';
+// import { Motion, spring } from 'react-motion';
 import { useSpring, animated } from 'react-spring';
 
-const AnimatedFeatureDetail = animated(FeatureDetail)
+// const AnimatedFeatureDetail = animated(FeatureDetail)
 
 function a11yProps(index) {
     return {
