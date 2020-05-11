e7b5102d1ca1765247b2588874eb9d3413b2f8db
zhangmk <51670196@qq.com>
2020-05-11 18:38:25 +0800

    Others map ExpansionPanel

---
+++ src/components/Rules/DataSrcSetter.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const DataSrcSetter = props => {
+
+    return <div>{JSON.stringify(props.data)}</div>
+}
+
+export default DataSrcSetter
--- src/components/Rules/Others.js
+++ src/components/Rules/Others.js
@@ -5,6 +5,7 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import DataSrcSetter from './DataSrcSetter';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,83 +22,42 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const menus = ['设置数据源', '设置结果存储表', '设置评分结果字段名', '设置评分标尺（刻度）', '设置变量分组比例']
-
-const Others = () => {
+const Others = props => {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
 
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
     };
+    const menus = [
+        { name: 'datasrc', cnname: '设置数据源', comp: <DataSrcSetter data={props.data.datasrc} /> },
+        { name: 'datatarget', cnname: '设置结果存储表', comp: <DataSrcSetter data={props.data.datatarget} /> },
+        { name: 'colname', cnname: '设置评分结果字段名', comp: <DataSrcSetter data={props.data.colname} /> },
+        { name: 'rulers', cnname: '设置评分标尺（刻度）', comp: <DataSrcSetter data={props.data.rulers} /> },
+        { name: 'catalog', cnname: '设置变量分组比例', comp: <DataSrcSetter data={props.data.catalog} /> },
+    ]
 
     return (
         <div className={classes.root}>
-            <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                <ExpansionPanelSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1bh-content"
-                    id="panel1bh-header"
-                >
-                    <Typography className={classes.heading}>{menus[0]}</Typography>
-                    <Typography className={classes.secondaryHeading}>dw_table1</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Typography>
-                        Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-                        maximus est, id dignissim quam.
-          </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-                <ExpansionPanelSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2bh-content"
-                    id="panel2bh-header"
-                >
-                    <Typography className={classes.heading}>{menus[1]}</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        dw_table2
-                    </Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Typography>
-                        Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-                        diam eros in elit. Pellentesque convallis laoreet laoreet.
-          </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-                <ExpansionPanelSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel3bh-content"
-                    id="panel3bh-header"
-                >
-                    <Typography className={classes.heading}>{menus[2]}</Typography>
-                    <Typography className={classes.secondaryHeading}>Col1</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Typography>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-                        vitae egestas augue. Duis vel est augue. </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-                <ExpansionPanelSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel4bh-content"
-                    id="panel4bh-header"
-                >
-                    <Typography className={classes.heading}>{menus[3]}</Typography>
-                    <Typography className={classes.secondaryHeading}>A:1 B:2</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Typography>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-                        vitae egestas augue. Duis vel est augue.
-          </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
+            {menus.map((m, i) =>
+                <ExpansionPanel expanded={expanded === `panel${i}`} onChange={handleChange(`panel${i}`)}>
+                    <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <Typography className={classes.heading}>{m.cnname}</Typography>
+                        <Typography className={classes.secondaryHeading}>{m.name}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        {/* <Typography>
+                            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+                        maximus est, id dignissim quam. </Typography> */}
+                        {m.comp}
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            )
+            }
         </div>
     );
 }
