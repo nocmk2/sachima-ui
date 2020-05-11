092723736c6abd973330918da1ffd42c5f757f2e
zhangmk <51670196@qq.com>
2020-05-11 17:46:49 +0800

    Rules => Others

--- src/components/Rules/Others.js
+++ src/components/Rules/Others.js
@@ -21,6 +21,8 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const menus = ['设置数据源', '设置结果存储表', '设置评分结果字段名', '设置评分标尺（刻度）', '设置变量分组比例']
+
 const Others = () => {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
@@ -37,8 +39,8 @@ const Others = () => {
                     aria-controls="panel1bh-content"
                     id="panel1bh-header"
                 >
-                    <Typography className={classes.heading}>General settings</Typography>
-                    <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+                    <Typography className={classes.heading}>{menus[0]}</Typography>
+                    <Typography className={classes.secondaryHeading}>dw_table1</Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <Typography>
@@ -53,10 +55,10 @@ const Others = () => {
                     aria-controls="panel2bh-content"
                     id="panel2bh-header"
                 >
-                    <Typography className={classes.heading}>Users</Typography>
+                    <Typography className={classes.heading}>{menus[1]}</Typography>
                     <Typography className={classes.secondaryHeading}>
-                        You are currently not an owner
-          </Typography>
+                        dw_table2
+                    </Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <Typography>
@@ -71,16 +73,13 @@ const Others = () => {
                     aria-controls="panel3bh-content"
                     id="panel3bh-header"
                 >
-                    <Typography className={classes.heading}>Advanced settings</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        Filtering has been entirely disabled for whole web server
-          </Typography>
+                    <Typography className={classes.heading}>{menus[2]}</Typography>
+                    <Typography className={classes.secondaryHeading}>Col1</Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <Typography>
                         Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-                        vitae egestas augue. Duis vel est augue.
-          </Typography>
+                        vitae egestas augue. Duis vel est augue. </Typography>
                 </ExpansionPanelDetails>
             </ExpansionPanel>
             <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
@@ -89,7 +88,8 @@ const Others = () => {
                     aria-controls="panel4bh-content"
                     id="panel4bh-header"
                 >
-                    <Typography className={classes.heading}>Personal data</Typography>
+                    <Typography className={classes.heading}>{menus[3]}</Typography>
+                    <Typography className={classes.secondaryHeading}>A:1 B:2</Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <Typography>
