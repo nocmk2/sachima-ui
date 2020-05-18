209f5f578507fe519a169c73775a073a82557dbc
nocmk2 <51670196@qq.com>
2020-05-18 16:15:33 +0800

    rulesAPI

--- src/api/rulesAPI.tsx
+++ src/api/rulesAPI.tsx
@@ -5,11 +5,26 @@ export interface RuleSummary {
     version: string
 }
 
-const getRuleLists = async (): Promise<RuleSummary[]> => {
-    const url = `${process.env.BASE_URL}/sachima/rules`
-    const options = {
-        headers: { Authorization: "Bearer " + localStorage.token }
-    }
-    const ruleListsResponse = await axios.get<RuleSummary[]>(url, options);
+export interface Rule extends RuleSummary {
+    rule: string
+}
+
+const OPTIONS = {
+    headers: { Authorization: "Bearer " + localStorage.token }
+}
+
+export const getRuleLists = async (): Promise<RuleSummary[]> => {
+    const url = `${process.env.REACT_APP_BASE_URL}/sachima/rules`
+    const ruleListsResponse = await axios.get<RuleSummary[]>(url, OPTIONS);
     return ruleListsResponse.data
+}
+
+export const getRule = async (name: string, version: string): Promise<Rule> => {
+    const url = `${process.env.REACT_APP_BASE_URL}/sachima/rule/${name}/${version}`
+    const rule = await axios.get<Rule>(url, OPTIONS);
+    return {
+        name: rule.data.name,
+        version: rule.data.version,
+        rule: JSON.parse(rule.data.rule)
+    }
 }
