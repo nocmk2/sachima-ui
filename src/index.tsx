5149b71755d2387e392b9ba76a43483c8159c7cb
nocmk2 <51670196@qq.com>
2020-05-18 11:25:27 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 
 import store from './app/store'
 
-// disable right click1
+// disable right click12
 document.oncontextmenu = function () {
     return false;
 }
