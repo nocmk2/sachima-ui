623691da20997cc976c48f6ec2d4030aae859f83
nocmk2 <51670196@qq.com>
2020-05-18 11:22:58 +0800

    修改index 使用ts 和redux

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import store from './app/store'
+
+// disable right click
+document.oncontextmenu = function () {
+    return false;
+}
+
+const render = () => {
+    const App = require('./App').default
+  
+    ReactDOM.render(
+      <Provider store={store}>
+         <App />
+      </Provider>,
+      document.getElementById('root')
+    )
+  }
+  
+render()
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('./App', render)
+  }
+
