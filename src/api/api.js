baea58b107dcdacd258960ebd9890afdda0bd5a9
nocmk2 <51670196@qq.com>
2020-05-28 14:27:02 +0800

    clean api

--- src/api/api.js
+++ src/api/api.js
@@ -1,47 +1,12 @@
 import React, { useState, useEffect, useReducer } from 'react'
 import axios from "axios"
 import { useInterceptor } from "utils/tools"
-// import { useStateValue } from "../utils/state"
-// import { useHistory } from "react-router-dom"
-
-// axios.interceptors.response.use(response => {
-//     return response;
-// }, error => {
-//     if (error.response.status === 401) {
-//         dispatch({
-//             type: "sendMessage",
-//             newMessage: { open: true, move: "left", info: `您没有权限,请登陆,或联系管理员${sachima.message}` }
-//         })
-//         history.push("/login")
-//     }
-//     // return error;
-// });
-
-// var OPTIONS = {}
-
-// Object.defineProperty(OPTIONS, "headers", {
-//     get: function () {
-//         return { Authorization: "Bearer " + localStorage.token }
-//     }
-// })
-
-// function getToken(callback) {
-//     let token = localStorage.token
-//     callback(token)
-// }
-
-
-// const OPTIONS = {
-//     headers: { Authorization: "Bearer " + getToken(token => token) }
-// }
 
 const OPTIONS = () => {
     return {
         headers: { Authorization: "Bearer " + localStorage.token }
     }
 }
-
-// console.log(Object.keys(OPTIONS))
 
 export const getRuleLists = async () => {
     const url = `${process.env.REACT_APP_BASE_URL}/sachima/rules`
@@ -153,22 +118,7 @@ const wrapPromise = (promise) => {
 }
 
 
-
-
-// { users: null, roles: null, objects: null, userrole: null, roleobject: null }
-// const initialState = { count: 0 };
-
-// const reducer = (state: ProfileDataType, action: any) => {
-//     switch (action.type) {
-//         case 'refresh':
-//             return { count: state. + 1 };
-//         default:
-//             throw new Error();
-//     }
-// }
-
 export const useFetchProfileData = () => {
-    // const [state, dispatch] = useReducer(reducer, null)
     const [count, setCount] = useState(0)
     const [data, setData] = useState(null)
 
@@ -177,33 +127,19 @@ export const useFetchProfileData = () => {
     }
 
     useInterceptor()
-    // useEffect(() => {
-    //     effect
-    //     return () => {
-    //         cleanup
-    //     }
-    // }, [input])
-
 
     useEffect(() => {
-        // let mounted = true
-        // if (mounted) {
         const users = wrapPromise(getUsers());
         const roles = wrapPromise(getRoles());
         const objects = wrapPromise(getObjects());
         const userRole = wrapPromise(getUserRole());
         const roleObject = wrapPromise(getRoleObjectAction());
         setData({ users, roles, objects, userRole, roleObject })
-        // } else {
-        //     setData(null)
-        // }
 
         return () => {
-            // mounted = false
         }
     }, [count])
 
-    //TODO: return [data, refresher]
     return [data, refresher, count]
 }
 
@@ -223,9 +159,5 @@ export const useImpromptu = (imps) => {
         })
 
     }, [])
-
-
-
-
 }
 
